d673e67965cc978e63e6d7f5393171bfe3b25236
Marina Bizzotto <37871563+MarinaGB@users.noreply.github.com>
2018-05-09 08:26:49 -0300

    Avaliação continuada P4
    Segue o trabalho referente a tabela ASCII do Professor Igor Peretta.

---
+++ P4.c
@@ -0,0 +1,25 @@
+// Marina Gautério Bizzotto
+// 11721EAU014
+#include <stdio.h>
+
+int main ()
+{
+	char bits[256];
+	int cum=0, i;
+	
+	printf ("Digite um numero: ");
+	scanf ("%s", bits);
+	getchar();
+	for (i=0; bits[i] != '\0'; i++)
+	{
+		if(bits[i] >=48 && bits[i]<=57)
+		{
+			bits[i]=bits[i]-'0';
+			cum= cum*10 + bits[i];
+		}
+	}
+	
+	printf ("O numero digitado: %d", cum);
+
+	return 0;
+}
